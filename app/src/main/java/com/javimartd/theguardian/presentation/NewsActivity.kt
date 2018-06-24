4cb175d1433188ee7a4884afc75eae08238e3ed0
Javi Martinez <javier.martinez@mo2o.com>
2018-06-24 16:20:08 +0200

    Initial commit

---
+++ app/src/main/java/com/javimartd/theguardian/presentation/NewsActivity.kt
@@ -0,0 +1,13 @@
+package com.javimartd.theguardian.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.javimartd.theguardian.R
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+    }
+}
