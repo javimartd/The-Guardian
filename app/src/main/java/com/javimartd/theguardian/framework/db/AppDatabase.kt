a4b9edab61e23251c5c2d65d3f4b6464d1619aac
javi martinez <javimartd@gmail.com>
2019-03-24 14:27:25 +0100

    chore: migrate room data base to android X

--- app/src/main/java/com/javimartd/theguardian/framework/db/AppDatabase.kt
+++ app/src/main/java/com/javimartd/theguardian/framework/db/AppDatabase.kt
@@ -1,7 +1,7 @@
 package com.javimartd.theguardian.framework.db
 
-import android.arch.persistence.room.Database
-import android.arch.persistence.room.RoomDatabase
+import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [NewsEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
