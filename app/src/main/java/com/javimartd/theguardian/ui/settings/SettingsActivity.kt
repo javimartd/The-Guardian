caba271aa72f995485600be66ae72fa269d5e422
javi martinez <javimartd@gmail.com>
2019-03-26 19:20:19 +0100

    chore: migrate settings to android X

--- app/src/main/java/com/javimartd/theguardian/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/javimartd/theguardian/ui/settings/SettingsActivity.kt
@@ -1,7 +1,7 @@
 package com.javimartd.theguardian.ui.settings
 
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
 import com.javimartd.theguardian.R
 import com.javimartd.theguardian.ui.base.ToolbarManager
 import com.javimartd.theguardian.ui.common.BaseActivity
