7b8d0e2880ff3411f66733cb278ae1c8a2c06fc1
javi martinez <javimartd@gmail.com>
2019-03-26 19:20:03 +0100

    chore: migrate webView to android X

--- app/src/main/java/com/javimartd/theguardian/ui/webView/WebViewActivity.kt
+++ app/src/main/java/com/javimartd/theguardian/ui/webView/WebViewActivity.kt
@@ -1,7 +1,7 @@
 package com.javimartd.theguardian.ui.webView
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.javimartd.theguardian.R
 import kotlinx.android.synthetic.main.activity_web_view.*
 
