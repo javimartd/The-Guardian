c487ebb8627de20d1347631b404d11c93b929c52
javi martinez <javimartd@gmail.com>
2019-03-26 19:22:30 +0100

    test: migrate tests to android X

--- app/src/androidTest/java/com/javimartd/theguardian/data/NewsEntityReadWriteTest.kt
+++ app/src/androidTest/java/com/javimartd/theguardian/data/NewsEntityReadWriteTest.kt
@@ -1,8 +1,8 @@
 package com.javimartd.theguardian.data
 
-import android.arch.persistence.room.Room
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.room.Room
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import com.javimartd.theguardian.framework.db.AppDatabase
 import com.javimartd.theguardian.framework.db.NewsDao
 import com.javimartd.theguardian.framework.db.NewsEntity
--- app/src/androidTest/java/com/javimartd/theguardian/ui/news/NewsActivityTest.kt
+++ app/src/androidTest/java/com/javimartd/theguardian/ui/news/NewsActivityTest.kt
@@ -1,17 +1,18 @@
 package com.javimartd.theguardian.ui.news
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.contrib.RecyclerViewActions
-import android.support.test.espresso.intent.Intents
-import android.support.test.espresso.intent.Intents.intended
-import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import android.support.test.espresso.matcher.ViewMatchers
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 import com.javimartd.theguardian.R
 import com.javimartd.theguardian.ui.settings.SettingsActivity
 import org.junit.Assert
