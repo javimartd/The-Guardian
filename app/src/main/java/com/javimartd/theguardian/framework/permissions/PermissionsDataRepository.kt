d5cd683dbbc63073e3db17f9f8fcad34f73eb8b6
javi martinez <javimartd@gmail.com>
2019-04-06 18:53:14 +0200

    chore: included permission repository

---
+++ app/src/main/java/com/javimartd/theguardian/framework/permissions/PermissionsDataRepository.kt
@@ -0,0 +1,21 @@
+package com.javimartd.theguardian.framework.permissions
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import com.javimartd.theguardian.domain.repositories.PermissionsRepository
+import javax.inject.Inject
+
+class PermissionsDataRepository @Inject constructor(val context: Context): PermissionsRepository {
+
+    override fun hasCameraPermission() = hasPermission(android.Manifest.permission.CAMERA)
+
+    override fun hasLocationPermission() = hasPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+    private fun hasPermission(permission: String): Boolean =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
+            } else {
+                true
+            }
+}
