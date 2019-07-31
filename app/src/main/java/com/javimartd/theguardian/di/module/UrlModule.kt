601362fce339aa6c5a7af275738d9682e8c722e6
javi martinez <javimartd@gmail.com>
2019-07-31 20:54:51 +0200

    chore: included url module

---
+++ app/src/main/java/com/javimartd/theguardian/di/module/UrlModule.kt
@@ -0,0 +1,17 @@
+package com.javimartd.theguardian.di.module
+
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+class UrlModule {
+
+    @Provides
+    @Singleton
+    @Named("baseUrl")
+    fun providesBaseUrl(): String {
+        return "https://content.guardianapis.com"
+    }
+}
